9711b80fcdd42c40265ac69044abd0587e81a352
amarcinkowski <andrzej.marcinkowski@gmail.com>
2016-09-26 13:14:17 +0200

    build from int array

--- src/main/java/io/github/amarcinkowski/tetromino/algorithm/CubeVolumeBuilder.java
+++ src/main/java/io/github/amarcinkowski/tetromino/algorithm/CubeVolumeBuilder.java
@@ -3,29 +3,25 @@ package io.github.amarcinkowski.tetromino.algorithm;
 public class CubeVolumeBuilder {
 
 	CubeVolume cubeVolume = CubeVolume.newInstance();
-	
+
 	public CubeVolumeBuilder empty() {
 		cubeVolume.cubeVolumePointer = 0;
 		cubeVolume.blockcount = 0;
-		for (int x = 0; x < CubeVolume.VOLUME; x++) {
+		for (int x = 0; x < CubeVolume.VOLUME_SIZE; x++) {
 			cubeVolume.filled[x] = 0;
 		}
 		return this;
 	}
-	
-/*
-	/// XXXX ????
+
 	public CubeVolumeBuilder filled(int[] filled) {
 		cubeVolume.cubeVolumePointer = 0;
 		cubeVolume.filled = filled;
-		cubeVolume.blockcount = 0; // ???
+		cubeVolume.blockcount = cubeVolume.getBlockList().size();
 		return this;
-	}*/
-	
+	}
+
 	public CubeVolume build() {
 		return cubeVolume;
 	}
-	
-	
-	
+
 }
